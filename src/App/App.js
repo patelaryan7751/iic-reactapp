97f779ebb90b292faf90979f965c440776f88cae
Chandrasekhar Samanta <chandrasekhar.2039@gmail.com>
2020-10-29 22:31:09 +0530

    Everything is ready to start development

--- src/App/App.js
+++ src/App/App.js
@@ -1,8 +1,12 @@
+import React from "react";
 import './App.css';
+
+import Navbar from "../Components/Navbar/navbar.js";
 
 function App() {
   return (
     <div className="App">
+           <Navbar />
         <h1>On development </h1>
     </div>
   );
